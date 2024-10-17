79ad8455a3e5c3ecbd866de73c26268886931a83
nknikkka <kulikveronika14@gmail.com>
2024-10-17 20:04:58 +0300

    Onovlenia 110

--- js/task-2.js
+++ js/task-2.js
@@ -1,65 +1,44 @@
-function calcAverageCalories(days) {
+function calcAverageCalories(weeklyCalories) {
+    if (weeklyCalories.length === 0) return 0;
+
+    let totalCalories = 0;
+
+    for (const entry of weeklyCalories) {
+        totalCalories += entry.calories;
+    }
+
+    return totalCalories / weeklyCalories.length;
+}
+function updateOutput(calories) {
     const outputDiv = document.getElementById('outputDiv');
-
-    if (days.length === 0) {
-        outputDiv.innerHTML += `<p>Average Calories: 0</p>`;
-        return 0;
-    }
-
-    const validDays = days.filter(day => day.calories !== undefined);
-
-    if (validDays.length === 0) {
-        outputDiv.innerHTML += `<p>Average Calories: 0</p>`;
-        return 0;
-    }
-
-    const totalCalories = validDays.reduce((sum, day) => {
-        let calories = day.calories;
-
-        if (day.intensity === 'high') 
-            {calories *= 1.1; } 
-        else if (day.intensity === 'low')
-            {calories *= 0.9;}
-
-        return sum + calories;
-    }, 0);
-
-    const averageCalories = totalCalories / validDays.length;
-
-    if (averageCalories > 3000) {
-        outputDiv.innerHTML += `<p>The athlete consumes enough calories. Average Calories: ${averageCalories.toFixed(2)}</p>`;} 
-    else if (averageCalories < 2000) {outputDiv.innerHTML += `<p>The athlete does not consume enough calories. Average Calories: ${averageCalories.toFixed(2)}</p>`;} 
-    else {outputDiv.innerHTML += `<p>Average Calories: ${averageCalories.toFixed(2)}</p>`;}
-
-    return averageCalories;
+    outputDiv.textContent = `Середнє значення калорій: ${calories}`;
 }
 
-let weeklyCalories = [
-    { day: "monday", calories: 3010, intensity: 'medium' },
-    { day: "tuesday", calories: 3200, intensity: 'high' },
-    { day: "wednesday", calories: 3120, intensity: 'low' },
-    { day: "thursday", calories: 2900, intensity: 'medium' },
-    { day: "friday", calories: 3450, intensity: 'high' },
-    { day: "saturday", calories: 3280, intensity: 'low' },
-    { day: "sunday", calories: 3300, intensity: 'medium' }
+let weeklyCalories1 = [
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
 ];
-calcAverageCalories(weeklyCalories);
 
-weeklyCalories = [
-    { day: "monday", calories: 2040, intensity: 'low' },
-    { day: "tuesday", calories: 2270, intensity: 'medium' },
-    { day: "wednesday", calories: 2420, intensity: 'high' },
-    { day: "thursday", calories: 1900, intensity: 'medium' },
-    { day: "friday", calories: 2370, intensity: 'low' },
-    { day: "saturday", calories: 2280, intensity: 'medium' },
-    { day: "sunday", calories: 2610, intensity: 'medium' }
+const averageCalories1 = calcAverageCalories(weeklyCalories1);
+updateOutput(averageCalories1);
+
+let weeklyCalories2 = [
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
 ];
-calcAverageCalories(weeklyCalories);
 
-weeklyCalories = [
-    { day: "monday", calories: 1400, intensity: 'low' },
-    { day: "tuesday", calories: 1600, intensity: 'low' }
-];
-calcAverageCalories(weeklyCalories);
+const averageCalories2 = calcAverageCalories(weeklyCalories2);
+updateOutput(averageCalories2); // Середнє значення калорій: 2270"
 
-calcAverageCalories([]);
+const averageCaloriesEmpty = calcAverageCalories([]);
+updateOutput(averageCaloriesEmpty); // Середнє значення калорій: 0"
--- js/task-3.js
+++ js/task-3.js
@@ -2,26 +2,26 @@ const profile = {
     username: "Jacob",
     playTime: 300,
 
-    changeUsername(newName)
-     { this.username = newName;},
+    changeUsername(newName) {
+        this.username = newName; },
 
-    updatePlayTime(hours) 
-    {this.playTime += hours;},
+    updatePlayTime(hours) {
+        this.playTime += hours; },
 
     getInfo() {
-        return `${this.username} has ${this.playTime} active hours!`;
-    }
+        return `${this.username} has ${this.playTime} active hours!`;}
 };
 
 function updateOutput() {
-    const outputDiv1 = document.getElementById('outputDiv1');
-    outputDiv1.textContent = profile.getInfo();
-}
+    const outputDiv = document.getElementById('outputDiv1');
+    
+    outputDiv.innerHTML = ''; 
 
-updateOutput();
+    outputDiv.innerHTML += profile.getInfo() + '<br>'; 
 
-profile.changeUsername("Marco");
-updateOutput();
+    profile.changeUsername("Marco");
+    outputDiv.innerHTML += profile.getInfo() + '<br>'; 
 
-profile.updatePlayTime(20);
-updateOutput(); 
+    profile.updatePlayTime(20);
+    outputDiv.innerHTML += profile.getInfo(); 
+}updateOutput();
